a8c50a910d4aa1cd149608a09094db6aa455affd
osblue13 <osblue13@gmail.com>
2016-10-12 12:22:48 +0100

    Store values in Redis

--- src/TodoApiCore/Data Layer/ITodoRepository.cs
+++ src/TodoApiCore/Data Layer/ITodoRepository.cs
@@ -10,7 +10,7 @@ namespace TodoApiCore.Models
         void Add(TodoItem item);
         IEnumerable<TodoItem> GetAll();
         TodoItem Find(string key);
-        TodoItem Remove(string key);
+        void Remove(string key);
         void Update(TodoItem item);
     }
 }
--- src/TodoApiCore/Data Layer/TodoRepository.cs
+++ src/TodoApiCore/Data Layer/TodoRepository.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using StackExchange.Redis;
+using Newtonsoft.Json;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,41 +11,59 @@ namespace TodoApiCore.Models
     public class TodoRepository : ITodoRepository
     {
         private static ConcurrentDictionary<string, TodoItem> _todos = new ConcurrentDictionary<string, TodoItem>();
+        private static ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+        private static IDatabase db = redis.GetDatabase();
+        private static string redisHashKey = "todoitems";
 
         public TodoRepository()
         {
-            Add(new TodoItem { Name = "Item1" });
-            Add(new TodoItem { Name = "Item2" });
+            Add(new TodoItem { Name = "Add Redis Support" });
+            Add(new TodoItem { Name = "Connect to a different container" });            
         }
 
         public IEnumerable<TodoItem> GetAll()
         {
-            return _todos.Values;
+            Dictionary<string, TodoItem> todos = new Dictionary<string, TodoItem>();            
+            var result = db.HashGetAll(redisHashKey);
+
+            foreach (var item in result)
+            {
+                todos.Add(item.Name, JsonConvert.DeserializeObject<TodoItem>(item.Value));
+            }
+
+            return todos.Values;
+
+            //return _todos.Values;
         }
 
         public void Add(TodoItem item)
         {
             item.Key = Guid.NewGuid().ToString();
-            _todos[item.Key] = item;
+            db.HashSet(redisHashKey, item.Key, JsonConvert.SerializeObject(item));
+
+            //_todos[item.Key] = item;
         }
 
         public TodoItem Find(string key)
         {
-            TodoItem item;
-            _todos.TryGetValue(key, out item);
-            return item;
+            return JsonConvert.DeserializeObject<TodoItem>(db.StringGet(key));
+                        
+            //_todos.TryGetValue(key, out item);
+            //return item;
         }
 
-        public TodoItem Remove(string key)
+        public void Remove(string key)
         {
-            TodoItem item;
-            _todos.TryRemove(key, out item);
-            return item;
+            //TodoItem item;
+            //_todos.TryRemove(key, out item);
+            //return item;
+
+            db.HashDelete(redisHashKey, key);
         }
 
         public void Update(TodoItem item)
         {
-            _todos[item.Key] = item;
+            db.HashSet(redisHashKey, item.Key, JsonConvert.SerializeObject(item));
         }
 
     }
